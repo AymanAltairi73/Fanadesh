b5fd45cdcce8ad59d4f5add695d7525f091953ee
Aymanaltairi73 <aymanaltairi7368@gmail.com>
2024-09-04 14:11:36 -0700

    first commit

---
+++ Models/Home.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace fenadesh.Models
+{
+    public class Home
+    {
+        [Required]
+        [Display]
+        public int Id { get; set; }
+        [Required]
+        [Display]
+        public string? Title { get; set; }
+        [Required]
+        [Display]
+        public string? Description { get; set; }
+        [Required]
+        [Display]
+        public string? ImageUrl { get; set; }
+
+    }
+}
